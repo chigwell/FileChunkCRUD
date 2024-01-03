13de989a92ae7db66bac2c61aa2cb3c34de67eed
Eugene Evstafiev <36392751+chigwell@users.noreply.github.com>
2024-01-03 20:41:56 +0000

    Add main logic and tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='FileChunkCRUD',
-    version='0.0.1',
+    version='0.0.2',
     description='Python package for CRUD operations on large files in chunks.',
     long_description=long_description,
     long_description_content_type='text/markdown',
